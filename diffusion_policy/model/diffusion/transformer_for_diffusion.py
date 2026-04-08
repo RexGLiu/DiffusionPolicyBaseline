6059a9363f58fb44e6e745bdf8528c11aa49bc66
RexGLiu <49000792+RexGLiu@users.noreply.github.com>
2026-04-08 19:14:34 -0400

    transformer_for_diffusion.py: Added architecture combining a convolutional image encoder with a diffusion transformer.

--- diffusion_policy/model/diffusion/transformer_for_diffusion.py
+++ diffusion_policy/model/diffusion/transformer_for_diffusion.py
@@ -2,10 +2,248 @@ from typing import Union, Optional, Tuple
 import logging
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from diffusion_policy.model.diffusion.positional_embedding import SinusoidalPosEmb
 from diffusion_policy.model.common.module_attr_mixin import ModuleAttrMixin
 
 logger = logging.getLogger(__name__)
+
+# Initialisation function taken from https://github.com/facebookresearch/gen_dgrl/tree/main/procgen.
+init_relu_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), nn.init.calculate_gain("relu"))
+
+def init(module, weight_init, bias_init, gain=1):
+    weight_init(module.weight.data, gain=gain)
+    bias_init(module.bias.data)
+    return module
+
+def apply_init_(modules):
+    """
+    Initialize NN modules
+    """
+    for m in modules:
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+            nn.init.constant_(m.weight, 1)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+
+class ConvEncoder(ModuleAttrMixin):
+    def __init__(self, observation_space, embed_dim, 
+                 channels=[32,64,64], fc_dim=1024, kernels=[8,4,3], strides=[4,2,1]):
+        """
+            observation_space: (H, W, C)
+            embed_dim: int
+            fc_dim: int
+            channels: list of channels, e.g. [32, 64, 64]
+            kernels: list of kernel sizes, e.g. [8, 4, 3]
+            strides: list of strides,      e.g. [4, 2, 1]
+        """
+        super(ConvEncoder, self).__init__()
+
+        assert len(channels) == len(kernels) == len(strides) == 3, f"Number of channels, kernels and strides must be 3. Received {channels} channels, {kernels} kernels, and {strides} strides."
+
+        self.observation_space = observation_space
+        self.embed_dim = embed_dim
+
+        self.conv1 = nn.Conv2d(observation_space.shape[-1], channels[0], kernels[0], stride=strides[0])
+        self.conv2 = nn.Conv2d(channels[0], channels[1], kernels[1], stride=strides[1])
+        self.conv3 = nn.Conv2d(channels[1], channels[2], kernels[2], stride=strides[2])
+        self.fc = nn.Linear(fc_dim, embed_dim)
+
+        self.apply_init_(self.modules())
+
+    def forward(self, x, t_embed=None):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+    
+    def apply_init_(self, modules):
+        """
+        Initialize NN modules
+        """
+        for m in modules:
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+
+class Conv2d_tf(nn.Conv2d):
+    """
+    Conv2d with the padding behavior from TF.
+    Adapted from https://github.com/facebookresearch/gen_dgrl/tree/main/procgen.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Conv2d_tf, self).__init__(*args, **kwargs)
+        self.padding = kwargs.get("padding", "SAME")
+
+    def _compute_padding(self, input, dim):
+        input_size = input.size(dim + 2)
+        filter_size = self.weight.size(dim + 2)
+        effective_filter_size = (filter_size - 1) * self.dilation[dim] + 1
+        out_size = (input_size + self.stride[dim] - 1) // self.stride[dim]
+        total_padding = max(0, (out_size - 1) * self.stride[dim] + effective_filter_size - input_size)
+        additional_padding = int(total_padding % 2 != 0)
+
+        return additional_padding, total_padding
+
+    def forward(self, input):
+        if self.padding == "VALID":
+            return F.conv2d(
+                input,
+                self.weight,
+                self.bias,
+                self.stride,
+                padding=0,
+                dilation=self.dilation,
+                groups=self.groups,
+            )
+        rows_odd, padding_rows = self._compute_padding(input, dim=0)
+        cols_odd, padding_cols = self._compute_padding(input, dim=1)
+        if rows_odd or cols_odd:
+            input = F.pad(input, [0, cols_odd, 0, rows_odd])
+
+        return F.conv2d(
+            input,
+            self.weight,
+            self.bias,
+            self.stride,
+            padding=(padding_rows // 2, padding_cols // 2),
+            dilation=self.dilation,
+            groups=self.groups,
+        )
+
+
+class BasicBlock(ModuleAttrMixin):
+    """
+    Residual Network Block.
+    Adapted from https://github.com/facebookresearch/gen_dgrl/tree/main/procgen.
+    """
+
+    def __init__(self, n_channels):
+        super(BasicBlock, self).__init__()
+
+        self.conv1 = Conv2d_tf(n_channels, n_channels, kernel_size=3, stride=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = Conv2d_tf(n_channels, n_channels, kernel_size=3, stride=1)
+
+        apply_init_(self.modules())
+
+        self.train()
+
+    def forward(self, x):
+        identity = x
+
+        out = self.relu(x)
+        out = self.conv1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        out += identity
+        return out
+
+
+class ResNetEncoderBlock(ModuleAttrMixin):
+    def __init__(self, in_channels, out_channels):
+        self.layer1 = Conv2d_tf(in_channels, out_channels, kernel_size=3, stride=1)
+        self.layer2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer3 = BasicBlock(out_channels)
+        self.layer4 = BasicBlock(out_channels)
+    
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        return x
+ 
+
+class ResNetEncoder(ModuleAttrMixin):
+    """
+    Residual Network from PPO implementation -> 1M parameters
+    Adapted from https://github.com/facebookresearch/gen_dgrl/tree/main/procgen.
+    """
+
+    def __init__(self, observation_space, hidden_size=256, channels=[16, 32, 32]):
+        super(ResNetEncoder, self).__init__()
+        self.observation_space = observation_space
+
+        self.block1 = ResNetEncoderBlock(observation_space.shape[-1], channels[0])
+        self.block2 = ResNetEncoderBlock(channels[0], channels[1])
+        self.block3 = ResNetEncoderBlock(channels[1], channels[2])
+
+        self.relu = nn.ReLU()
+
+        self.fc = init_relu_(nn.Linear(2048, hidden_size))
+
+        apply_init_(self.modules())
+
+        self.train()
+
+    def forward(self, inputs):
+        x = inputs
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+
+        x = x.reshape(x.size(0), -1)
+        x = self.relu(x)
+        x = self.relu(self.fc(x))
+
+        return x
+    
+
+class ResNet20Encoder(ModuleAttrMixin):
+    """
+    Residual Network with 20 layers -> 35M parameters
+    Adapted from https://github.com/facebookresearch/gen_dgrl/tree/main/procgen.
+    """
+
+    def __init__(self, observation_space, hidden_size=256, channels=[64, 256, 256, 512]):
+        super(ResNet20Encoder, self).__init__()
+        self.observation_space = observation_space
+        
+        self.block1 = ResNetEncoderBlock(observation_space.shape[-1], channels[0])
+        self.block2 = ResNetEncoderBlock(channels[0], channels[1])
+        self.block3 = ResNetEncoderBlock(channels[1], channels[2])
+        self.block4 = ResNetEncoderBlock(channels[2], channels[3])
+
+        self.relu = nn.ReLU()
+
+        self.fc1 = init_relu_(nn.Linear(8192, 2048))
+        self.fc2 = init_relu_(nn.Linear(2048, hidden_size))
+
+        apply_init_(self.modules())
+
+        self.train()
+
+    def forward(self, inputs):
+        x = inputs
+
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+
+        x = x.reshape(x.size(0), -1)
+        x = self.relu(x)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+
+        return x
+   
 
 class TransformerForDiffusion(ModuleAttrMixin):
     def __init__(self,
@@ -342,6 +580,125 @@ class TransformerForDiffusion(ModuleAttrMixin):
         x = self.head(x)
         # (B,T,n_out)
         return x
+
+
+class TransformerWithEncoderForDiffusion(ModuleAttrMixin):
+    def __init__(self,
+            obs_encoder: ModuleAttrMixin,
+            transformer: TransformerForDiffusion,
+        ) -> None:
+        super().__init__()
+
+        self.obs_encoder = obs_encoder
+        self.transformer = transformer
+
+        # init
+        logger.info(
+            "number of parameters: %e", sum(p.numel() for p in self.parameters())
+        )
+
+    def encode_obs(self, 
+        sample: torch.Tensor) -> torch.Tensor:
+        """
+        sample: (B,T,*input_dim)
+        """
+        B,T,C,H,W = sample.shape
+        sample = sample.reshape(B*T,C,H,W)  # fold T into the batch dim
+
+        observations = self.obs_encoder(sample)
+
+        assert len(observations.shape) == 2
+        observations = observations.reshape(B,T,-1)   # unfold T from batch dim
+
+        return observations
+
+    def forward(self, 
+        sample: torch.Tensor, 
+        timestep: Union[torch.Tensor, float, int], 
+        cond: torch.Tensor, **kwargs) -> torch.Tensor:
+        """
+        sample: (B,T,input_dim)
+        timestep: (B,) or int, diffusion step
+        cond: (B,T',C,H,W)
+        output: (B,T,input_dim)
+        """
+
+        cond = self.encode_obs(cond)
+        actions = self.transformer(sample, timestep, cond)
+
+        return actions
+    
+    def get_optim_groups(self, weight_decay: float=1e-3):
+        """
+        This long function is unfortunately doing something very simple and is being very defensive:
+        We are separating out all parameters of the model into two buckets: those that will experience
+        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+        We are then returning the PyTorch optimizer object.
+        """
+
+        # separate out all parameters to those that will and won't experience regularizing weight decay
+        decay = set()
+        no_decay = set()
+        whitelist_weight_modules = (torch.nn.Linear, torch.nn.MultiheadAttention, torch.nn.Conv2d)
+        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding, torch.nn.MaxPool2d)
+        for mn, m in self.named_modules():
+            for pn, p in m.named_parameters():
+                fpn = "%s.%s" % (mn, pn) if mn else pn  # full param name
+
+                if pn.endswith("bias"):
+                    # all biases will not be decayed
+                    no_decay.add(fpn)
+                elif pn.startswith("bias"):
+                    # MultiheadAttention bias starts with "bias"
+                    no_decay.add(fpn)
+                elif pn.endswith("weight") and isinstance(m, whitelist_weight_modules):
+                    # weights of whitelist modules will be weight decayed
+                    decay.add(fpn)
+                elif pn.endswith("weight") and isinstance(m, blacklist_weight_modules):
+                    # weights of blacklist modules will NOT be weight decayed
+                    no_decay.add(fpn)
+
+        # special case the position embedding parameter in the root GPT module as not decayed
+        no_decay.add("pos_emb")
+        no_decay.add("_dummy_variable")
+        if self.cond_pos_emb is not None:
+            no_decay.add("cond_pos_emb")
+
+        # validate that we considered every parameter
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_params = decay | no_decay
+        assert (
+            len(inter_params) == 0
+        ), "parameters %s made it into both decay/no_decay sets!" % (str(inter_params),)
+        assert (
+            len(param_dict.keys() - union_params) == 0
+        ), "parameters %s were not separated into either decay/no_decay set!" % (
+            str(param_dict.keys() - union_params),
+        )
+
+        # create the pytorch optimizer object
+        optim_groups = [
+            {
+                "params": [param_dict[pn] for pn in sorted(list(decay))],
+                "weight_decay": weight_decay,
+            },
+            {
+                "params": [param_dict[pn] for pn in sorted(list(no_decay))],
+                "weight_decay": 0.0,
+            },
+        ]
+        return optim_groups
+
+    def configure_optimizers(self, 
+            learning_rate: float=1e-4, 
+            weight_decay: float=1e-3,
+            betas: Tuple[float, float]=(0.9,0.95)):
+        optim_groups = self.get_optim_groups(weight_decay=weight_decay)
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=betas
+        )
+        return optimizer
 
 
 def test():
